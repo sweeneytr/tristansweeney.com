5f996d98eca49e527dedfda60676f4b21079c2e0
Tristan Sweeney <sweeneytri@gmail.com>
2024-08-28 16:38:53 -0400

    Fix acer post

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://tristansweeney.com/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  site: process.env.VERCEL_ENV === 'production' ? 'https://tristansweeney.com/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  }), mdx()],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
-  },
+      exclude: ['@resvg/resvg-js']
+    }
+  }
 });
