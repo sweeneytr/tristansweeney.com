34784f66b13c63dfd7ffb5df1e6914c7c7689f6a
Tristan Sweeney <sweeneytri@gmail.com>
2024-08-30 17:38:56 -0400

    Refactors

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,43 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
 import mdx from "@astrojs/mdx";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site: process.env.VERCEL_ENV === 'production' ? 'https://tristansweeney.com/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
-  trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({
-    injectReset: true
-  }), mdx()],
+  site:
+    process.env.VERCEL_ENV === "production"
+      ? "https://tristansweeney.com/"
+      : process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}/`
+        : "https://localhost:3000/",
+  trailingSlash: "ignore",
+  integrations: [
+    sitemap(),
+    UnoCSS({
+      injectReset: true,
+    }),
+    expressiveCode({
+      themes: ["github-light-high-contrast"],
+      styleOverrides: {
+        // You can optionally override the plugin's default styles here
+        frames: {
+          shadowColor: "red",
+          frameBoxShadowCssValue: "7px 7px 0 rgb(0 0 0 / 1);"
+        },
+        borderWidth: "3px",
+        borderColor: "black",
+        
+      },
+    }),
+    mdx(),
+  ],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js']
-    }
-  }
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,19 @@
 import { z, defineCollection } from 'astro:content';
 
-const blogCollection = defineCollection({
+const blog = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      author: z.string(),
+      tags: z.array(z.string()),
+      description: z.string(),
+      pubDate: z.string().transform((str) => new Date(str)),
+      imgUrl: image(),
+      draft: z.boolean().optional().default(false),
+    }),
+});
+
+const projects = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
@@ -14,5 +27,6 @@ const blogCollection = defineCollection({
 });
 
 export const collections = {
-  blog: blogCollection,
+  blog,
+  projects,
 };
