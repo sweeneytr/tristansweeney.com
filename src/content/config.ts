c53f7b521011a57c3cf84780e2ba9b1d8f5198de
Tristan Sweeney <sweeneytri@gmail.com>
2024-08-31 14:51:04 -0400

    Fix build

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,34 +1,21 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, type ImageFunction } from "astro:content";
 
-const credit = z.object({author: z.string(), url: z.string()});
+const credit = z.object({ author: z.string(), url: z.string() });
+const schema = ({ image }: { image: ImageFunction }) =>
+  z.object({
+    title: z.string(),
+    author: z.string(),
+    tags: z.array(z.string()),
+    description: z.string(),
+    pubDate: z.string().transform((str) => new Date(str)),
+    imgUrl: image(),
+    credit: credit.optional(),
+    draft: z.boolean().optional().default(false),
+  });
 
-const blog = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      author: z.string(),
-      tags: z.array(z.string()),
-      description: z.string(),
-      pubDate: z.string().transform((str) => new Date(str)),
-      imgUrl: image(),
-      credit: credit.optional(),
-      draft: z.boolean().optional().default(false),
-    }),
-});
+const blog = defineCollection({ schema });
 
-const projects = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      author: z.string(),
-      tags: z.array(z.string()),
-      description: z.string(),
-      pubDate: z.string().transform((str) => new Date(str)),
-      imgUrl: image(),
-      credit: credit.optional(),
-      draft: z.boolean().optional().default(false),
-    }),
-});
+const projects = defineCollection({ schema });
 
 export const collections = {
   blog,
