00250c1dbec9c38eaaaa2b1d1461172bce605351
Tristan Sweeney <sweeneytri@gmail.com>
2025-03-09 23:43:06 -0400

    Define colors better

--- src/colors.ts
+++ src/colors.ts
@@ -1,18 +1,18 @@
 export const colors = [
-  "#c084fc",
-  "#f472b6",
-  "#fb7185",
-  "#e879f9",
-  "#a78bfa",
-  "#818cf8",
-  "#60a5fa",
-  "#38bdf8",
-  "#22d3ee",
-  "#2dd4bf",
-  "#34d399",
-  "#4ade80",
-  "#a3e635",
-  "#facc15",
-  "#fb923c",
-  "#f87171",
+  "#c084fc", // Lavender (Floral) - violet
+  "#f472b6", // Hot Pink - rose
+  "#fb7185", // Bright pink (Crayola) - fuchsia
+  "#e879f9", // Heliotrope - pink
+  "#a78bfa", // Tropical indigo - purple
+  "#818cf8", // Also tropical indigo
+  "#60a5fa", // Ruddy Blue - blue
+  "#38bdf8", // Deep Sky Blue - sky
+  "#22d3ee", // Vivid Sky Blue - cyan
+  "#2dd4bf", // Turquoise - teal
+  "#34d399", // Emerald - green
+  "#4ade80", // Emerald - emerald
+  "#a3e635", // Spring bud - lime
+  "#facc15", // Jonquil - yellow
+  "#fb923c", // Dark Orange (web) - orange
+  "#f87171", // Light Red - Red
 ];
