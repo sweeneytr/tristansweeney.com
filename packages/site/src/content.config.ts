504e6e931c45733ac31e516f4b241bd48e12b426
Tristan Sweeney <sweeneytri@gmail.com>
2026-03-05 22:13:04 -0500

    Set up catalog, merge posts back in

--- packages/site/src/content.config.ts
+++ packages/site/src/content.config.ts
@@ -1,6 +1,30 @@
-import { PostData } from "@aelar/posts";
 import { glob } from "astro/loaders";
-import { defineCollection } from "astro:content";
+import { defineCollection, z, type ImageFunction } from "astro:content";
+
+const PostData = ({ image }: { image: ImageFunction }) => {
+  const imageSchema = z.object({
+    url: image(),
+    credit: z.object({ author: z.string(), url: z.string() }).optional(),
+    style: z
+      .object({
+        "object-fit": z.string().optional(),
+        "background-color": z.string().optional(),
+      })
+      .optional(),
+  });
+
+  return z.object({
+    title: z.string(),
+    author: z.string(),
+    tags: z.array(z.string()),
+    description: z.string(),
+    pubDate: z.string().transform((str) => new Date(str)),
+    image: imageSchema.optional(),
+    draft: z.boolean().optional().default(false),
+  });
+};
+
+export type PostData = z.infer<ReturnType<typeof PostData>>;
 
 const blog = defineCollection({
   schema: PostData,
