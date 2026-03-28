cb18f8af494e0220ffc8ea3fa1035d1c14d01a27
Tristan Sweeney <sweeneytri@gmail.com>
2026-03-27 21:18:18 -0400

    Set content disposition explicitly

--- packages/site/src/pages/v1/generate/resume.pdf.ts
+++ packages/site/src/pages/v1/generate/resume.pdf.ts
@@ -6,15 +6,14 @@ import fs from "fs/promises";
 import os from "os";
 
 export const GET: APIRoute = async ({ site }) => {
-  if (site) {
-    (await loadFonts()).forEach(ReactPDF.Font.register);
-  }
+  (await loadFonts()).forEach(ReactPDF.Font.register);
 
   await ReactPDF.render(Resume(), `${os.tmpdir()}/resume.pdf`);
 
   return new Response(await fs.readFile(`${os.tmpdir()}/resume.pdf`), {
     headers: {
       "content-type": "application/pdf",
+      "Content-Disposition": "attachment; filename=TristanSweeneyResume.pdf",
     },
   });
 };
